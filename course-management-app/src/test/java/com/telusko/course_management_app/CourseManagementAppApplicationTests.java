0bca1b28197c17505af4dd986c4165e1c0f51c24
Vansh Mahalle <vanshmahale055@gmail.com>
2026-06-18 20:43:12 +0530

    Initial Spring Boot project setup

---
+++ course-management-app/src/test/java/com/telusko/course_management_app/CourseManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.course_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
